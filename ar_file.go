0370fb51fbd576f96eba0106ec22a96a62fcb33b
Jan Voung <jvoung@chromium.org>
2014-01-02 14:09:49 -0800

    Fix long filenames in archive, etc. Skip symbol table in archive. Test.

--- ar_file.go
+++ ar_file.go
@@ -6,6 +6,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -37,19 +38,36 @@ type ARFileHeaderContents struct {
 
 type ARFile map[string] ARFileHeaderContents
 
+func translateFilename(fname string, lf_file []byte) string {
+	if fname[0] == '/' {
+		// It's a long filename, which is /[0-9]+, or one of the special files.
+		fname = strings.TrimSpace(fname)
+		if fname == "/" || fname == "//" {
+			return fname
+		}
+		offset, err := strconv.Atoi(fname[1:])
+		if err != nil {
+			panic("Failed to parse long filename offset " + err.Error())
+		}
+		end := bytes.IndexByte(lf_file[offset:], '/')
+		return string(lf_file[offset : offset + end])
+	} else {
+		end := strings.IndexByte(fname, '/')
+		return fname[:end]
+	}
+}
+
 func ReadPlainARFile(f *os.File) ARFile {
 	ar_file := make(map[string] ARFileHeaderContents)
 	per_file_header_size := 60
 	hbuf := make([]byte, per_file_header_size)
 	// Assume magic number header is already read.
 	offset := int64(len(AR_MAGIC))
-	var special_long_filename_file []byte
+	special_long_filename_file := make([]byte, 0)
 	// Go through the AR, reading more and more file-headers + file-bodies.
 	for {
 		n, err := f.ReadAt(hbuf, offset)
-		if err == io.EOF {
-			fmt.Printf("Hit eof, reading %d chars, which is %s\n", n,
-				hbuf[0])
+		if err == io.EOF && n == 0 {
 			break
 		}
 		if err != nil {
@@ -62,9 +80,8 @@ func ReadPlainARFile(f *os.File) ARFile {
 		if err != nil {
 			panic("Failed to parse AR file size: " + err.Error())
 		}
-		// TODO(jvoung): Handle the / which is the terminator, instead of using
-		// TrimSpace. Also use the / to handle long filenames.
-		filename := strings.TrimSpace(string(hbuf[0:16]))
+		filename := translateFilename(string(hbuf[0:16]),
+			special_long_filename_file)
 		new_header := ARFileHeader {
 			Filename: filename,
 			Timestamp: strings.TrimSpace(string(hbuf[16:28])),
@@ -77,16 +94,24 @@ func ReadPlainARFile(f *os.File) ARFile {
 		if err2 != nil {
 			panic("Failed to read AR sub-file contents: " + err2.Error())
 		}
-		fmt.Println("Read stuff for", new_header.String())
 		if filename == "/" {
-			fmt.Println("Copied", copy(special_long_filename_file, body_buf))
+			// Skipping the special GNU symbol-table file.
+			// (not adding it to the ar_file map)
+		} else if filename == "//" {
+			// This is the long-filename file.
+			// (not adding it to the ar_file map)
+			special_long_filename_file = append(special_long_filename_file,
+				body_buf...)
 		} else {
+			// Normal file, index it!
 			ar_file[filename] = ARFileHeaderContents{new_header, body_buf}
 		}
 		offset += int64(fsize)
+		// Data section should be aligned to 2 bytes.
+		if offset % 2 != 0 {
+			offset += 1
+		}
 	}
-	fmt.Println("Special long filename section:",
-		string(special_long_filename_file))
 	return ar_file
 }
 
--- ar_file_test.go
+++ ar_file_test.go
@@ -11,9 +11,14 @@ import (
 	"testing"
 )
 
-func TestARFileStructure(t *testing.T) {
+// Test an archive with elf files.
+// This has a GNU symbol table.
+func TestELFARFileStructure(t *testing.T) {
 	test_name := path.Join(TestX8632BaseDir(), "libcrt_platform.a")
-	expected_subfiles := []string{"pnacl_irt.o", "setjmp.o", "string.o"}
+	expected_subfiles := map[string]bool{
+		"pnacl_irt.o": true,
+		"setjmp.o": true,
+		"string.o": true}
 	f, err := os.Open(test_name)
 	if err != nil {
 		t.Fatal("Failed to open test AR file")
@@ -21,9 +26,40 @@ func TestARFileStructure(t *testing.T) {
 	defer f.Close()
 	ar_file := ReadPlainARFile(f)
 	ExpectEq(t, len(expected_subfiles), len(ar_file))
-	// TODO(jvoung): Check more...
+	// Check that the contents are really ELF.
+	for fname, hdr_contents := range ar_file {
+		ExpectEq(t, string(hdr_contents.Contents[0:4]), ELF_MAGIC)
+		ExpectEq(t, expected_subfiles[fname], true)
+	}
 }
 
+// Test an archive with text files.
+// This has long filenames and files with a space in the name.
 func TestLongFilenames(t *testing.T) {
-	// TODO(jvoung): test...
+	test_name := path.Join(TestLibDir(), "liblong_filename.a")
+	expected_subfiles := []string{
+		"file_11.txt",
+		"file_24.txt",
+		"file_nil.txt",
+		"file_quick_brown_fox_jumped.txt",
+		"file with space in it.txt"}
+	expected_contents := map[string]string{
+		"file_11.txt": "0123456789\n",
+		"file_24.txt": "55555\n55555\n55555\n55555\n",
+		"file_nil.txt": "",
+		"file_quick_brown_fox_jumped.txt": "the quick brown fox " +
+			"jumps over the lazy dog\n",
+		"file with space in it.txt": "This file has a space in its name.\n"}
+	f, err := os.Open(test_name)
+	if err != nil {
+		t.Fatal("Failed to open test AR file")
+	}
+	defer f.Close()
+	ar_file := ReadPlainARFile(f)
+	ExpectEq(t, len(expected_subfiles), len(ar_file))
+	for fname, hdr_contents := range ar_file {
+		ec := expected_contents[fname]
+		ExpectEq(t, uint32(len(ec)), hdr_contents.Header.FileSize)
+		ExpectEq(t, ec, string(hdr_contents.Contents))
+	}
 }
