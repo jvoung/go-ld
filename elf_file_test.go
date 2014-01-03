b3d96a9e197f9a35933b258284338e47762f93e9
Jan Voung <jvoung@chromium.org>
2014-01-03 14:06:46 -0800

    Test x86-64 and ARM section headers too.

--- elf_file_test.go
+++ elf_file_test.go
@@ -56,6 +56,69 @@ func TestRelocatableELFX8632(t *testing.T) {
 			Sh_addr: 0, Sh_offset: 0x40, Sh_size: 0x100,
 			Sh_link: 0, Sh_info: 0,	Sh_addralign: 32, Sh_entsize: 0},
 		elf_file.Shdrs[2])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 1, Sh_name: ".rel.text",
+			Sh_type: elf.SHT_REL,
+			Sh_flags: 0,
+			Sh_addr: 0, Sh_offset: 0x5fc, Sh_size: 0x20,
+			Sh_link: 10, Sh_info: 2, Sh_addralign: 4, Sh_entsize: 8},
+		elf_file.Shdrs[3])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 75, Sh_name: ".data",
+			Sh_type: elf.SHT_PROGBITS,
+			Sh_flags: elf.SHF_WRITE | elf.SHF_ALLOC,
+			Sh_addr: 0, Sh_offset: 0x140, Sh_size: 0,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 4, Sh_entsize: 0},
+		elf_file.Shdrs[4])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 11, Sh_name: ".bss",
+			Sh_type: elf.SHT_NOBITS,
+			Sh_flags: elf.SHF_WRITE | elf.SHF_ALLOC,
+			Sh_addr: 0, Sh_offset: 0x140, Sh_size: 0x40,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 8, Sh_entsize: 0},
+		elf_file.Shdrs[5])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 81, Sh_name: ".note.NaCl.ABI.x86-32",
+			Sh_type: elf.SHT_NOTE,
+			Sh_flags: elf.SHF_ALLOC | elf.SHF_GROUP,
+			Sh_addr: 0, Sh_offset: 0x140, Sh_size: 0x1c,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 4, Sh_entsize: 0},
+		elf_file.Shdrs[6])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 39, Sh_name: ".eh_frame",
+			Sh_type: elf.SHT_PROGBITS,
+			Sh_flags: elf.SHF_WRITE | elf.SHF_ALLOC,
+			Sh_addr: 0, Sh_offset: 0x15c, Sh_size: 0,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 4, Sh_entsize: 0},
+		elf_file.Shdrs[7])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 23, Sh_name: ".note.GNU-stack",
+			Sh_type: elf.SHT_PROGBITS,
+			Sh_flags: 0,
+			Sh_addr: 0, Sh_offset: 0x15c, Sh_size: 0,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 1, Sh_entsize: 0},
+		elf_file.Shdrs[8])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 49, Sh_name: ".shstrtab",
+			Sh_type: elf.SHT_STRTAB,
+			Sh_flags: 0,
+			Sh_addr: 0, Sh_offset: 0x15c, Sh_size: 0x67,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 1, Sh_entsize: 0},
+		elf_file.Shdrs[9])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 67, Sh_name: ".symtab",
+			Sh_type: elf.SHT_SYMTAB,
+			Sh_flags: 0,
+			Sh_addr: 0, Sh_offset: 0x3a4, Sh_size: 0x120,
+			Sh_link: 11, Sh_info: 13, Sh_addralign: 4, Sh_entsize: 0x10},
+		elf_file.Shdrs[10])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 59, Sh_name: ".strtab",
+			Sh_type: elf.SHT_STRTAB,
+			Sh_flags: 0,
+			Sh_addr: 0, Sh_offset: 0x4c4, Sh_size: 0x136,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 1, Sh_entsize: 0},
+		elf_file.Shdrs[11])
 }
 
 func TestRelocatableELFX8664(t *testing.T) {
@@ -84,6 +147,87 @@ func TestRelocatableELFX8664(t *testing.T) {
 	ExpectEq(t, uint16(9), elf_file.Header.Shstrndx)
 	ExpectEq(t, 0, len(elf_file.Phdrs))
 	ExpectEq(t, 12, len(elf_file.Shdrs))
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 0, Sh_name: "", Sh_type: elf.SHT_NULL,
+			Sh_flags: 0, Sh_addr: 0, Sh_offset: 0, Sh_size: 0,
+			Sh_link: 0, Sh_info: 0,	Sh_addralign: 0, Sh_entsize: 0},
+		elf_file.Shdrs[0])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 17, Sh_name: ".group",
+			Sh_type: elf.SHT_GROUP,
+			Sh_flags: 0, Sh_addr: 0, Sh_offset: 0x40, Sh_size: 8,
+			Sh_link: 10, Sh_info: 2, Sh_addralign: 4, Sh_entsize: 4},
+		elf_file.Shdrs[1])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 6, Sh_name: ".text",
+			Sh_type: elf.SHT_PROGBITS,
+			Sh_flags: elf.SHF_ALLOC | elf.SHF_EXECINSTR,
+			Sh_addr: 0, Sh_offset: 0x60, Sh_size: 0x160,
+			Sh_link: 0, Sh_info: 0,	Sh_addralign: 32, Sh_entsize: 0},
+		elf_file.Shdrs[2])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 1, Sh_name: ".rela.text",
+			Sh_type: elf.SHT_RELA,
+			Sh_flags: 0,
+			Sh_addr: 0, Sh_offset: 0x830, Sh_size: 0xc0,
+			Sh_link: 10, Sh_info: 2, Sh_addralign: 8, Sh_entsize: 0x18},
+		elf_file.Shdrs[3])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 76, Sh_name: ".data",
+			Sh_type: elf.SHT_PROGBITS,
+			Sh_flags: elf.SHF_WRITE | elf.SHF_ALLOC,
+			Sh_addr: 0, Sh_offset: 0x1c0, Sh_size: 0,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 4, Sh_entsize: 0},
+		elf_file.Shdrs[4])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 12, Sh_name: ".bss",
+			Sh_type: elf.SHT_NOBITS,
+			Sh_flags: elf.SHF_WRITE | elf.SHF_ALLOC,
+			Sh_addr: 0, Sh_offset: 0x1c0, Sh_size: 0x40,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 8, Sh_entsize: 0},
+		elf_file.Shdrs[5])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 82, Sh_name: ".note.NaCl.ABI.x86-64",
+			Sh_type: elf.SHT_NOTE,
+			Sh_flags: elf.SHF_ALLOC | elf.SHF_GROUP,
+			Sh_addr: 0, Sh_offset: 0x1c0, Sh_size: 0x1c,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 4, Sh_entsize: 0},
+		elf_file.Shdrs[6])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 40, Sh_name: ".eh_frame",
+			Sh_type: elf.SHT_PROGBITS,
+			Sh_flags: elf.SHF_WRITE | elf.SHF_ALLOC,
+			Sh_addr: 0, Sh_offset: 0x1dc, Sh_size: 0,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 4, Sh_entsize: 0},
+		elf_file.Shdrs[7])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 24, Sh_name: ".note.GNU-stack",
+			Sh_type: elf.SHT_PROGBITS,
+			Sh_flags: 0,
+			Sh_addr: 0, Sh_offset: 0x1dc, Sh_size: 0,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 1, Sh_entsize: 0},
+		elf_file.Shdrs[8])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 50, Sh_name: ".shstrtab",
+			Sh_type: elf.SHT_STRTAB,
+			Sh_flags: 0,
+			Sh_addr: 0, Sh_offset: 0x1dc, Sh_size: 0x68,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 1, Sh_entsize: 0},
+		elf_file.Shdrs[9])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 68, Sh_name: ".symtab",
+			Sh_type: elf.SHT_SYMTAB,
+			Sh_flags: 0,
+			Sh_addr: 0, Sh_offset: 0x548, Sh_size: 0x1b0,
+			Sh_link: 11, Sh_info: 13, Sh_addralign: 8, Sh_entsize: 0x18},
+		elf_file.Shdrs[10])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 60, Sh_name: ".strtab",
+			Sh_type: elf.SHT_STRTAB,
+			Sh_flags: 0,
+			Sh_addr: 0, Sh_offset: 0x6f8, Sh_size: 0x136,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 1, Sh_entsize: 0},
+		elf_file.Shdrs[11])
 }
 
 func TestRelocatableELFARM(t *testing.T) {
@@ -111,6 +255,88 @@ func TestRelocatableELFARM(t *testing.T) {
 	ExpectEq(t, uint16(9), elf_file.Header.Shstrndx)
 	ExpectEq(t, 0, len(elf_file.Phdrs))
 	ExpectEq(t, 12, len(elf_file.Shdrs))
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 0, Sh_name: "", Sh_type: elf.SHT_NULL,
+			Sh_flags: 0, Sh_addr: 0, Sh_offset: 0, Sh_size: 0,
+			Sh_link: 0, Sh_info: 0,	Sh_addralign: 0, Sh_entsize: 0},
+		elf_file.Shdrs[0])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 32, Sh_name: ".group",
+			Sh_type: elf.SHT_GROUP,
+			Sh_flags: 0, Sh_addr: 0, Sh_offset: 0x34, Sh_size: 8,
+			Sh_link: 10, Sh_info: 5, Sh_addralign: 4, Sh_entsize: 4},
+		elf_file.Shdrs[1])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 5, Sh_name: ".text",
+			Sh_type: elf.SHT_PROGBITS,
+			Sh_flags: elf.SHF_ALLOC | elf.SHF_EXECINSTR,
+			Sh_addr: 0, Sh_offset: 0x40, Sh_size: 0xe8,
+			Sh_link: 0, Sh_info: 0,	Sh_addralign: 16, Sh_entsize: 0},
+		elf_file.Shdrs[2])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 1, Sh_name: ".rel.text",
+			Sh_type: elf.SHT_REL,
+			Sh_flags: 0,
+			Sh_addr: 0, Sh_offset: 0x63c, Sh_size: 0x40,
+			Sh_link: 10, Sh_info: 2, Sh_addralign: 4, Sh_entsize: 8},
+		elf_file.Shdrs[3])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 94, Sh_name: ".data",
+			Sh_type: elf.SHT_PROGBITS,
+			Sh_flags: elf.SHF_WRITE | elf.SHF_ALLOC,
+			Sh_addr: 0, Sh_offset: 0x128, Sh_size: 0,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 4, Sh_entsize: 0},
+		elf_file.Shdrs[4])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 11, Sh_name: ".bss",
+			Sh_type: elf.SHT_NOBITS,
+			Sh_flags: elf.SHF_WRITE | elf.SHF_ALLOC,
+			Sh_addr: 0, Sh_offset: 0x128, Sh_size: 0x40,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 8, Sh_entsize: 0},
+		elf_file.Shdrs[5])
+	SHT_ARM_ATTRIBUTES := elf.SectionType(0x70000003)
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 16, Sh_name: ".ARM.attributes",
+			Sh_type: SHT_ARM_ATTRIBUTES,
+			Sh_flags: 0,
+			Sh_addr: 0, Sh_offset: 0x128, Sh_size: 0x26,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 1, Sh_entsize: 0},
+		elf_file.Shdrs[6])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 39, Sh_name: ".note.NaCl.ABI.arm",
+			Sh_type: elf.SHT_NOTE,
+			Sh_flags: elf.SHF_ALLOC | elf.SHF_GROUP,
+			Sh_addr: 0, Sh_offset: 0x150, Sh_size: 0x18,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 4, Sh_entsize: 0},
+		elf_file.Shdrs[7])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 58, Sh_name: ".eh_frame",
+			Sh_type: elf.SHT_PROGBITS,
+			Sh_flags: elf.SHF_WRITE | elf.SHF_ALLOC,
+			Sh_addr: 0, Sh_offset: 0x168, Sh_size: 0,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 4, Sh_entsize: 0},
+		elf_file.Shdrs[8])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 68, Sh_name: ".shstrtab",
+			Sh_type: elf.SHT_STRTAB,
+			Sh_flags: 0,
+			Sh_addr: 0, Sh_offset: 0x168, Sh_size: 0x64,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 1, Sh_entsize: 0},
+		elf_file.Shdrs[9])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 86, Sh_name: ".symtab",
+			Sh_type: elf.SHT_SYMTAB,
+			Sh_flags: 0,
+			Sh_addr: 0, Sh_offset: 0x3ac, Sh_size: 0x150,
+			Sh_link: 11, Sh_info: 16, Sh_addralign: 4, Sh_entsize: 0x10},
+		elf_file.Shdrs[10])
+	ExpectEq(t,
+		SectionHeader{Sh_name_index: 78, Sh_name: ".strtab",
+			Sh_type: elf.SHT_STRTAB,
+			Sh_flags: 0,
+			Sh_addr: 0, Sh_offset: 0x4fc, Sh_size: 0x13f,
+			Sh_link: 0, Sh_info: 0, Sh_addralign: 1, Sh_entsize: 0},
+		elf_file.Shdrs[11])
 }
 
 // Check a particular executable ELF file for a particular architecture
